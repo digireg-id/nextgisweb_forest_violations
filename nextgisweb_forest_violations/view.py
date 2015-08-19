a47ea4bf8d447fa5868090746d147875520a19fa
drnextgis <rykovd@gmail.com>
2015-08-20 01:02:57 +0600

    Рекурсивный обход связанных документов

--- nextgisweb_forest_violations/view.py
+++ nextgisweb_forest_violations/view.py
@@ -8,9 +8,9 @@ from pyramid.response import Response
 
 from nextgisweb.resource import DataScope
 from nextgisweb.vector_layer import VectorLayer
-from nextgisweb.feature_layer.api import serialize
+from nextgisweb.feature_layer.api import serialize as wkt_serialize
 
-DOCUMENTS_STACK = (
+DOCUMENTS = (
     ('docs', 'Документы'),
     ('sheet', 'Ведомости пересчета'),
     ('production', 'Лесопродукция'),
@@ -21,11 +21,11 @@ PERM_READ = DataScope.read
 PERM_WRITE = DataScope.write
 
 
-def gj_serialize(feat):
+def serialize(feat):
     """Сериализация объекта, геометрия в GeoJSON
     """
 
-    sfeat = serialize(feat)
+    sfeat = wkt_serialize(feat)
     sfeat['geom'] = feat.geom.__geo_interface__
     return sfeat
 
@@ -34,46 +34,54 @@ def getdoc(context, request):
     """Возвращаем документ и связанные данные
     """
 
-    resource = VectorLayer.filter_by(keyname='docs').one()
-    request.resource_permission(PERM_READ, resource)
+    p_srs = request.params.get('srs')
+    p_traversal = request.params.get('traversal')
 
     fid = request.matchdict.get('fid')
-    if fid is None:
-        return None
+    if fid is None: return None
 
     # Запрашиваемая система координат
-    p_srs = request.params.get('srs')
     srsid = int(p_srs) if p_srs else 4326
     srs = Bunch(id=srsid)
 
-    query = resource.feature_query()
-    query.geom()
+    # Нужен ли рекурсивный обход
+    traversal = int(p_traversal) if p_traversal else 1
 
-    if hasattr(query, 'srs'):
+    def traverse(id):
+        resource = VectorLayer.filter_by(keyname='docs').one()
+        request.resource_permission(PERM_READ, resource)
+
+        query = resource.feature_query()
+        query.filter_by(id=id)
+        query.limit(1)
         query.srs(srs)
+        query.geom()
 
-    query.filter_by(id=fid)
-    query.limit(1)
+        for doc in query():
+            result = serialize(doc)
 
-    result = None
-    for doc in query():
-        result = gj_serialize(doc)
-        result['related'] = {}
-        for key, display_name in DOCUMENTS_STACK:
-            result['related'][key] = []
+            result['related'] = {}
+            for key, name in DOCUMENTS:
+                result['related'][key] = []
 
-            resource = VectorLayer.filter_by(keyname=key).one()
-            request.resource_permission(PERM_READ, resource)
-            
-            query = resource.feature_query()
-            query.geom()
+                resource = VectorLayer.filter_by(keyname=key).one()
+                request.resource_permission(PERM_READ, resource)
 
-            if hasattr(query, 'srs'):
+                query = resource.feature_query()
+                query.filter_by(doc_id=doc.id)
                 query.srs(srs)
+                query.geom()
 
-            query.filter_by(doc_id=doc.id)
-            for f in query():
-                result['related'][key].append(gj_serialize(f))
+                for reldoc in query():
+                    if key == 'docs' and traversal:
+                        result['related'][key].append(traverse(reldoc.id))
+                    else:
+                        result['related'][key].append(serialize(reldoc))
+
+        return result
+
+    # Определяем связанные документы
+    result = traverse(fid)
 
     return Response(
         json.dumps(result),
